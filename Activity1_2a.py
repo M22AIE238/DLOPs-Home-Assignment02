1dfcc385c4924a41e5f7f8dcca8b62092ac8183d
M22AIE238 <m22aie238@iitj.ac.in>
2024-04-03 00:24:09 +0530

    added the modifications

--- Activity1_2a.py
+++ Activity1_2a.py
@@ -14,3 +14,34 @@ for val, sigmoid_val in zip(random_values, sigmoid_values):
     print(f"Sigmoid({val}) = {sigmoid_val}")
 
 
+import numpy as np
+
+# Given data
+random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
+
+# ReLU function
+def ReLU(x):
+    return np.maximum(0, x) 
+
+# Leaky ReLU function
+def Leaky_ReLU(x, alpha=0.01):
+    return np.where(x > 0, x, alpha * x)
+
+# Tanh function
+def Tanh(x):
+    return np.tanh(x)
+
+# Calculate outputs for each function
+ReLU_values = ReLU(np.array(random_values))
+Leaky_ReLU_values = Leaky_ReLU(np.array(random_values))
+Tanh_values = Tanh(np.array(random_values))
+
+# Print the results
+print("ReLU values:")
+print(Leaky_ReLU_values) ## A bug is created by wrong printing wrong values
+print("\nLeaky ReLU values:")
+print(ReLU_values)
+print("\nTanh values:")
+print(Tanh_values)
+
+
